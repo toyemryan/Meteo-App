9b973ba4c9e869ca7c3b4922d486ff3081e845d7
Djouaka kelefack lionel <djouakalionel4@gmail.com>
2022-09-02 22:22:36 +0200

    Process Layout

---
+++ app/src/main/java/com/example/meteoapp/Process_Image_Main.kt
@@ -0,0 +1,26 @@
+package com.example.meteoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class Process_Image_Main : AppCompatActivity() {
+
+    private lateinit var processimage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_process_image_main)
+
+
+        processimage = findViewById(R.id.imageView)
+        processimage.alpha =0.5f
+        processimage.animate().setDuration(5000).alpha(1f).withEndAction {
+            val intent = Intent(this, MainActivity :: class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
